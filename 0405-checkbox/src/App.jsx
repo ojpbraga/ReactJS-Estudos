a76198c2a63e33ba4d6a6304073fe4c61529cc61
João Pedro O. Braga <ojpbraga@gmail.com>
2024-08-02 21:35:22 -0300

    class: checkbox

---
+++ 0405-checkbox/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+function App() {
+
+  // Checkbox
+  // O estado do checkbox está relacionado ao checked
+  const [terms, setTerms] = useState(false);
+
+  // Multiplos
+  // Podemos definir um estado para cada item ou uma array que irá conter todos os itens selecionados
+  const [colors, setColors] = useState(['red']);
+
+  function handleChange({target}) {
+    if(target.checked) {
+      setColors([...colors, target.value]);
+      console.log(colors)
+    } else {
+      setColors(colors.filter((color) => color !== target.value));
+      console.log(colors)
+    }
+  }
+
+  // Verifica dentro do useState se já tem alguma opção por default.
+  function handleChecked(color) {
+    return colors.includes(color);
+  }
+
+  return (
+    <>
+      <form action="">
+        <label>
+          <input 
+            type="checkbox" 
+            name="" 
+            id=""
+            value="terms"
+            checked={terms}
+            onChange={({target}) => setTerms(target.checked)}
+          />
+          Accept terms
+        </label>
+        <p>{terms ? "Accepted" : "Denied"}</p>
+      </form>
+
+      <form action="">
+        <label>
+          <input 
+            type="checkbox"
+            value="blue"
+            checked={handleChecked("blue")}
+            onChange={handleChange}
+          />
+          Blue
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="red"
+            checked={handleChecked("red")}
+            onChange={handleChange}
+          />
+          Red
+        </label>
+      </form>
+
+    </>
+  )
+}
+
+export default App
