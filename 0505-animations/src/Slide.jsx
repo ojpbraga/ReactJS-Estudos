2e6ecd7774c7b5a78d9a5c8238d98fe1c06177cc
João Pedro O. Braga <ojpbraga@gmail.com>
2024-08-21 16:51:46 -0300

    class: animations II

--- 0505-animations/src/Slide.jsx
+++ 0505-animations/src/Slide.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './Slide.module.css'
 
 function Slide({slides}) {
+    const [active, setActive] = useState(0);
+    const [position, setPosition] = useState(0);
+    const contentRef = useRef();
+
+    useEffect(() => {
+        const { width } = contentRef.current.getBoundingClientRect();
+        setPosition(width * active);
+    }, [active]);
+
+    function slidePrev() {
+        if(active > 0) setActive(active - 1);
+    }
+
+    function slideNext() {
+        if(active < slides.length - 1) setActive(active + 1);
+    }
 
 
-    
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
+        <div ref={contentRef} className={styles.content} style={{transform: `translateX(-${position}px)`}}>
             {slides.map(({text, id}) => (
                 <div className={styles.item} key={id}>
                     {text}
@@ -15,8 +30,8 @@ function Slide({slides}) {
             ))}
         </div>
         <nav className={styles.nav}>
-            <button>Previus</button>
-            <button>Next</button>
+            <button onClick={slidePrev}>Previus</button>
+            <button onClick={slideNext}>Next</button>
         </nav>
     </section>
   )
