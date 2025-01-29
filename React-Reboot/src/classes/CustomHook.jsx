c6391b88d60e1ee83dfd6b6b6651858a02d53fac
João Pedro O. Braga <ojpbraga@gmail.com>
2025-01-29 17:17:34 -0300

    Reboot: Custom Hook

--- React-Reboot/src/classes/CustomHook.jsx
+++ React-Reboot/src/classes/CustomHook.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (key, initial) => {
@@ -11,16 +11,67 @@ const useLocalStorage = (key, initial) => {
     return[state, setState];
 }
 
+const useFetch = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(null);
+
+    const doRequest = useCallback(async (url, options) => {
+        let response;
+        let json;
+
+        try {
+            setError(false);
+            setLoading(true);
+            response = await fetch(url, options);
+            json = await response.json();
+        } catch (error) {
+            json = null;
+            setError(true);
+        } finally {
+            setData(json);
+            setLoading(false);
+            return {response, json}
+        }
+    }, []);
+
+    return {data, loading, doRequest, error}
+}
+
 const CustomHook = () => {
     // Podemos criar nossos proprios hooks para evitar repetição de código. Deve começar com a palavra use.
     const [product, setProduct] = useLocalStorage('produto', 'notebook');
+    const {doRequest, data, loading, error} = useFetch();
 
-    return (
-        <div>
-            <h1>Custom Hook</h1>
-            <button onClick={({target}) => setProduct(target.innerText)}>Notebook</button>
-            <button onClick={({target}) => setProduct(target.innerText)}>Celular</button>
-        </div>
-    )
+    useEffect(() => {
+        async function fetchData() {
+            const {response, json} = await doRequest('https://ranekapi.origamid.dev/json/api/produto/');
+        }
+        fetchData();
+        console.log('Fetch') // app é atualizados inúmeras vezes, ou seja, toda vez é uma função nova
+    }, [doRequest]);
+
+    if(error) return <p>Erro ao carregar</p> 
+    if (loading) return <p>Carregando</p>
+    if (data) 
+        return (
+            <div>
+                <h1>Custom Hook</h1>
+                <button onClick={({target}) => setProduct(target.innerText)}>Notebook</button>
+                <button onClick={({target}) => setProduct(target.innerText)}>Celular</button>
+        
+                <h3>Lista de produtos</h3>
+                <ul>
+                    {
+                        data && data.map(({nome, id}) => (
+                            <li key={id}>{nome}</li>
+                        ))
+                    }
+                </ul>
+
+            </div>
+        )
+    
+    
 }
 export default CustomHook;
